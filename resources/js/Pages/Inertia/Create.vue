50636237617e0d237a6af8198a61af0c6297b657
諸井孝平 <moroikouhei@moroikouheis-MacBook-Air.local>
2023-01-06 09:28:59 +0900

    inertia-vue

---
+++ resources/js/Pages/Inertia/Create.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { reactive } from 'vue';
+import { Inertia } from '@inertiajs/inertia';
+
+defineProps({
+    errors: Object
+})
+const form = reactive({
+    title: null,
+    content: null
+})
+
+const submitFunciton = () =>{
+    Inertia.post('/inertia', form)
+}
+
+</script>
+
+<template>
+    <form @submit.prevent="submitFunciton">
+        <input type="text" name="title" v-model="form.title"><br>
+        <div v-if="errors.title">{{ errors.title }}</div>
+        <input type="text" name="content" v-model="form.content"><br>
+        <div v-if="errors.content">{{ errors.content }}</div>
+    <button>送信</button>
+    </form>
+
+</template>
